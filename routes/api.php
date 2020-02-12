9053057fccac225a7f848089d3acdee34941783b
Tony Blanco <tblancog@gmail.com>
2020-02-12 20:15:03 -0300

    Created authenticated endpoint to get a board given columns and rows

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,21 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Route::middleware('auth:api')->prefix('/game')->group( function() {
+Route::prefix('/game')->group( function() {
+
+  // Returns a new board with requested cols and rows with zeroed values
+  Route::get('new', function() {
+    $cols = request()->get('rows');
+    $rows = request()->get('cols');
+
+    $board[][] = [[]];
+    for($y = 0; $y < $cols; $y++) {
+      for($x = 0; $x < $rows; $x++) {
+        $board[$y][$x] = 0;  
+      }
+    }
+    return $board;
+  });
+}
+);
