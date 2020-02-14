e183db32129316754a1e7ea311cb0c90bd680f50
Tony Blanco <tblancog@gmail.com>
2020-02-14 14:18:52 -0300

    Set name of the app and stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>BombSweeper</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,21 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BombSweeper
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <p>
+                <div>
+                  <a href="{{ route('login') }}">Log in</a> as:
+                </div>
+                <div>
+                 User: <strong>johndoe@email.com</strong>
+                 Password:<strong>johndoe@email.com</strong> 
+                </div>
+                <br/>
+                  - OR -
+                <p>
+                <div>
+                <a href="{{ route('register') }}">Register to access</a><p>
                 </div>
             </div>
         </div>
